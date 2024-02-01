706aab9a339c7427ece3fbdb8383e73a9ec9c089
Tricerat0x <alex@automy.io>
2024-02-01 18:27:20 -0500

    Initial commit

---
+++ stocksArray.js
@@ -0,0 +1,84 @@
+export const stocksArray = [
+    {
+      "name": "Innovatech Solutions",
+      "ticker": "INVS",
+      "industry": "Technology",
+      "marketCap": "50B",
+      "sharePrice": 250,
+      "beta": 1.2
+    },
+    {
+      "name": "GreenFuture Energy",
+      "ticker": "GFEN",
+      "industry": "Renewable Energy",
+      "marketCap": "30B",
+      "sharePrice": 150,
+      "beta": 1.4
+    },
+    {
+      "name": "MediCare Health",
+      "ticker": "MDCH",
+      "industry": "Healthcare",
+      "marketCap": "20B",
+      "sharePrice": 100,
+      "beta": 0.9
+    },
+    {
+      "name": "Oceanic Exploration",
+      "ticker": "OCEN",
+      "industry": "Oil & Gas",
+      "marketCap": "45B",
+      "sharePrice": 180,
+      "beta": 1.5
+    },
+    {
+      "name": "Quantum Financials",
+      "ticker": "QFNC",
+      "industry": "Financial Services",
+      "marketCap": "55B",
+      "sharePrice": 300,
+      "beta": 1.1
+    },
+    {
+      "name": "CyberSafe Security",
+      "ticker": "CSSE",
+      "industry": "Cybersecurity",
+      "marketCap": "25B",
+      "sharePrice": 125,
+      "beta": 1.3
+    },
+    {
+      "name": "BlueSky Airlines",
+      "ticker": "BSAL",
+      "industry": "Airlines",
+      "marketCap": "15B",
+      "sharePrice": 75,
+      "beta": 1.6
+    },
+    {
+      "name": "EcoSustain Materials",
+      "ticker": "ESMT",
+      "industry": "Manufacturing",
+      "marketCap": "40B",
+      "sharePrice": 200,
+      "beta": 1.0
+    },
+    {
+      "name": "NextGen Telecom",
+      "ticker": "NGTL",
+      "industry": "Telecommunications",
+      "marketCap": "35B",
+      "sharePrice": 170,
+      "beta": 1.2
+    },
+    {
+      "name": "FreshFarm Foods",
+      "ticker": "FFFD",
+      "industry": "Consumer Goods",
+      "marketCap": "10B",
+      "sharePrice": 50,
+      "beta": 0.8
+    }
+  ]
+  
+
